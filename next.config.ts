433d103d2fc1b1f0b58b2dbb169e269af5e511cd
szymonuryga <szymon.uryga9@gmail.com>
2025-02-13 13:05:08 +0100

    Image Loader

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-}
+  images: {
+    minimumCacheTTL: 600,
+    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      }
+    ],
+    loader: 'custom',
+    loaderFile: './lib/image/loader.ts'
+  },
+  // Fix for image loader, bug in next.js source code
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'next/dist/shared/lib/image-loader': './lib/image/loader.ts'
+      }
+    }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'self'" }
+        ]
+      }
+    ];
+  }
+};
 
-export default nextConfig
+export default nextConfig;
