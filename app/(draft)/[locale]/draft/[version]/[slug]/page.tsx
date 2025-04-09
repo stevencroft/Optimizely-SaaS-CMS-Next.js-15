33dcca9f5e0a2e36d6f5dd21511393ead92d382f
szymonuryga <szymon.uryga9@gmail.com>
2025-04-09 12:11:30 +0200

    allow draft mode on localhost

--- app/(draft)/[locale]/draft/[version]/[slug]/page.tsx
+++ app/(draft)/[locale]/draft/[version]/[slug]/page.tsx
@@ -2,7 +2,7 @@ import ContentAreaMapper from '@/components/content-area/mapper'
 import OnPageEdit from '@/components/draft/on-page-edit'
 import { optimizely } from '@/lib/optimizely/fetch'
 import { getValidLocale } from '@/lib/optimizely/utils/language'
-import { draftMode } from 'next/headers'
+import { checkDraftMode } from '@/lib/utils/draft-mode'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
@@ -12,7 +12,7 @@ export const dynamic = 'force-dynamic'
 export default async function CmsPage(props: {
   params: Promise<{ locale: string; version: string; slug?: string }>
 }) {
-  const { isEnabled: isDraftModeEnabled } = await draftMode()
+  const isDraftModeEnabled = await checkDraftMode()
   if (!isDraftModeEnabled) {
     return notFound()
   }
--- app/(draft)/[locale]/draft/[version]/experience/[key]/page.tsx
+++ app/(draft)/[locale]/draft/[version]/experience/[key]/page.tsx
@@ -3,7 +3,7 @@ import VisualBuilderExperienceWrapper from '@/components/visual-builder/wrapper'
 import { optimizely } from '@/lib/optimizely/fetch'
 import { SafeVisualBuilderExperience } from '@/lib/optimizely/types/experience'
 import { getValidLocale } from '@/lib/optimizely/utils/language'
-import { draftMode } from 'next/headers'
+import { checkDraftMode } from '@/lib/utils/draft-mode'
 import { notFound } from 'next/navigation'
 import { Suspense } from 'react'
 
@@ -13,7 +13,7 @@ export const dynamic = 'force-dynamic'
 export default async function Page(props: {
   params: Promise<{ key: string; locale: string; version: string }>
 }) {
-  const { isEnabled: isDraftModeEnabled } = await draftMode()
+  const isDraftModeEnabled = await checkDraftMode()
   if (!isDraftModeEnabled) {
     return notFound()
   }
